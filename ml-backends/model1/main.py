222962625032cfe6e8693d7c41755f85d944548f
Divij-Agarwal-42 <divij18a@gmail.com>
2026-03-03 20:01:57 -0500

    Sample model1 example

--- ml-backends/model1/main.py
+++ ml-backends/model1/main.py
@@ -1,5 +1,11 @@
-import sleep
+from time import sleep
+import numpy as np
 
-for i in range(1, 100):
-    print("this is model1, iteration:", i)
-    sleep(0.5)
+def dot(a, b):
+    return np.dot(a,b)
+
+print("Starting model1 processing...")
+for i in range(1, 10):
+    print("Dot product:", dot(np.array([i, i+1]), np.array([i+2, i+3])))
+    sleep(0.3)
+print("Finished processing model1")
